c212527bc9dad19ede7177b1f571c0c6658048a6
DrantDumani <darnjames@gmail.com>
2024-07-24 22:10:22 -0400

    Refactored routes to MVC methodology

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 const port = process.env.PORT || 3000;
