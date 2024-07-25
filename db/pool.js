f3cd7e86ec1ab088158f50c6f232f80c36b0eafa
Darnell <darnjames@gmail.com>
2024-07-25 19:59:03 -0400

    Fix env variable references

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,8 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const { HOST, USER, DATABASE, PASSWORD, DB_PORT } = process.env;
+const { URI } = process.env;
 
 module.exports = new Pool({
-  connectionString: `postgresql://${USER}:${PASSWORD}@${HOST}:${DB_PORT}/${DATABASE}`,
+  connectionString: URI,
 });
